003c3741da45f1dbdd8cb5a83296bd67a95fc618
Albert Penticoss <apenticoss@c02rp0gvgg7f.ltu.edu.au>
2019-05-10 14:52:15 +1000

    css to external repo

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 echo -e "\033[0;32mBuilding site...\033[0m"
 
 #delete previous build
+echo -e "\033[0;32mDelete previous build...\033[0m"
 rm -rf docs
+#find ../design-system-site -not -name '.*' ! -path '../design-system-site/.*' -delete
 
 #build Site
 hugo
