0d84bc032576fea2ddc9e08068094301acf79423
Albert Penticoss <a.penticoss@latrobe.edu.au>
2019-05-14 14:49:50 +1000

    latest build

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 echo -e "\033[0;32mStarting sass watch and hugo server...\033[0m"
 
-sass --watch --no-source-map static/ltucss/sass:static/ltucss/css & hugo server && fg
+sass --watch --no-source-map static/ltucss/sass:static/ltucss/css & hugo server --disableFastRender && fg
