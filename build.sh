f8e11aa4a340def437c60521d85a23323d95bbd0
Albert Penticoss <a.penticoss@latrobe.edu.au>
2019-05-14 12:23:25 +1000

    latest

--- build.sh
+++ build.sh
@@ -7,5 +7,9 @@ echo -e "\033[0;32mDelete previous build...\033[0m"
 rm -rf docs
 #find ../design-system-site -not -name '.*' ! -path '../design-system-site/.*' -delete
 
+#compile css for website
+echo -e "\033[0;32mCompile sass...\033[0m"
+sass --no-source-map static/ltucss/sass:static/ltucss/css
+
 #build Site
 hugo
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "\033[0;32mStarting sass watch and hugo server...\033[0m"
+
+sass --watch --no-source-map static/ltucss/sass:static/ltucss/css & hugo server && fg
